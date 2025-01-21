ec2ece249998573dc917ce0efb88492bad9bd10e
r03i22 <103725932+r03i22@users.noreply.github.com>
2025-01-21 10:48:22 +0900

    style: font linkを移動

--- app/root.tsx
+++ app/root.tsx
@@ -17,7 +17,6 @@ export function Layout({ children }: { children: React.ReactNode }): ReactNode {
         <meta name="google-site-verification" content="brDkeRhoxktrjCiqqUefNlNyOKLGHk0Cik9q9MzLv2E" />
         <Meta />
         <Links />
-        <link href="https://fonts.googleapis.com/css2?family=Irish+Grover&display=swap" rel="stylesheet"></link>
         <title>2025 鈴鹿高専卒業ライブ</title>
       </head>
       <body style={{ margin: 0, padding: 0, }}>
@@ -45,6 +44,10 @@ export const links: LinksFunction = () => {
     {
       rel: "stylesheet",
       href: "https://fonts.googleapis.com/css2?family=Roboto&display=swap",
+    },
+    {
+      rel: "stylesheet",
+      href: "https://fonts.googleapis.com/css2?family=Irish+Grover&display=swap",
     },
   ];
 };
