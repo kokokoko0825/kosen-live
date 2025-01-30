31341da53d7f4591b1ab1df23412395cca34999d
r03i22 <103725932+r03i22@users.noreply.github.com>
2025-01-30 20:21:50 +0900

    feat: 情報ページの内容を更新し、スタイルを調整

--- app/component/Information/Information.tsx
+++ app/component/Information/Information.tsx
@@ -35,9 +35,14 @@ export function Information(): ReactNode {
             </div>
             <div className={styles.infoContainer}>
                 <div className={styles.list}>
-                    <div className={styles.date}>2025.01.25</div>
+                    <div className={styles.date}>2025.01.30</div>
                     <div className={styles.newest}>new!</div>
-                    <div className={styles.textEng}>Day my dream 参戦決定!</div>
+                    <div className={styles.textEng}>MAD STMACHACHES, &nbsp;The Beginner &nbsp;参戦決定!</div>
+                </div>
+                <div className={styles.list}>
+                    <div className={styles.date}>2025.01.25</div>
+                    <div className={styles.newest}>    </div>
+                    <div className={styles.textEng}>Day my dream &nbsp;参戦決定!</div>
                 </div>
                 <div className={styles.list}>
                     <div className={styles.date}>2025.01.21</div>
